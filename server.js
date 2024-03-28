76ff955c8b7e44a36e8e76d0a47fcc2a411734d3
nurulislamrimon <nurulislamrimon@gmail.com>
2024-03-28 23:42:22 +0600

    store dynamic route created

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const fs = require("fs");
 
 const app = express();
+dotenv.config();
+
+const stripeSecrecKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+
+// routes==========================
+app.get("/store", (req, res) => {
+  fs.readFile("items.json", (err, data) => {
+    if (err) {
+      res.status(500).send("Error loading items");
+    } else {
+      res.render("store.ejs", {
+        items: JSON.parse(data),
+      });
+    }
+  });
+});
+
+// listen
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
